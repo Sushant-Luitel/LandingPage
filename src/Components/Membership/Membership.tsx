7131a6c2309295d9a7c464c3e30e2848c6620962
Pranawa <pranawakc187@gmail.com>
2025-11-18 20:23:04 +0545

    feat(blog-memberShip):fix blog)

--- src/Components/Membership/Membership.tsx
+++ src/Components/Membership/Membership.tsx
@@ -1,67 +1,27 @@
 import { useState } from "react";
+import { memberShip } from "./MembershipData";
+
 
 function Membership() {
-  const [toggle, setToggle] = useState("monthly");
-  const memberShip = [
-    {
-      title: "Free",
-      price: "$0",
-      priceSubtext: "",
-      yearlyPrice: "$0",
-      yearlySubtext: "",
-      description: "Access to all free articles",
-      list: [
-        "Access to limited Articles",
-        "Articles with Advertising",
-        "No card required",
-      ],
-      button: "Sign Up for free",
-    },
-
-    {
-      title: "Premium",
-      price: "$5",
-      priceSubtext: "/ Month",
-      yearlyPrice: "$50",
-      yearlySubtext: "/ Year",
-      description: "Full Access to Premium Content",
-      list: [
-        "Access to premium posts",
-        "Weekly newsletters",
-        "Simple, secure card payment",
-        "No Advertising",
-      ],
-      button: "Sign Up Now",
-    },
-
-    {
-      title: "Golden+✨",
-      price: "$9",
-      priceSubtext: "/ Month",
-      yearlyPrice: "$90",
-      yearlySubtext: "/ Year",
-      description: "Access to all premium content and special offers",
-      list: ["Access to premium posts", "Weekly newsletters", "No Advertising"],
-      button: "Sign Up Now",
-    },
-  ];
+  const [pricingPeriod, setPricingPeriod] = useState("monthly");
+  
 
   return (
     <>
       <div className="flex flex-col gap-[50px] px-5 items-center">
         <div className="w-[450px] flex justify-around items-center  py-3 rounded-full  shadow-2xl">
           <button
-            onClick={() => setToggle("monthly")}
+            onClick={() => setPricingPeriod("monthly")}
             className={`px-20 py-3 rounded-full text-[20px] ${
-              toggle === "monthly" ? "bg-red-500 text-white font-semibold" : "bg-transparent text-[#454630]"
+              pricingPeriod === "monthly" ? "bg-red-500 text-white font-semibold" : "bg-transparent text-[#454630]"
             }`}
           >
             Monthly
           </button>
           <button
-            onClick={() => setToggle("yearly")}
+            onClick={() => setPricingPeriod("yearly")}
             className={`px-20 py-3 rounded-3xl text-[20px] ${
-              toggle === "yearly" ? "bg-red-500 text-white font-semibold" : "bg-transparent text-[#454630]"
+              pricingPeriod === "yearly" ? "bg-red-500 text-white font-semibold" : "bg-transparent text-[#454630]"
             }`}
           >
             Yearly
@@ -78,9 +38,9 @@ function Membership() {
                 {membership.title}
               </div>
               <div className="text-[55px] font-sans text-red-500 font-bold mb-[15px]">
-                {toggle === "monthly" ? membership.price : membership.yearlyPrice}
+                {pricingPeriod === "monthly" ? membership.price : membership.yearlyPrice}
                 <span className="text-[15px] text-[#454630]  lg:text-[17px]">
-                  {toggle === "monthly" ? membership.priceSubtext : membership.yearlySubtext}
+                  {pricingPeriod === "monthly" ? membership.priceSubtext : membership.yearlySubtext}
                 </span>
               </div>
               <div className="text-center text-[13px] font-sans text-[#454630] mb-[25px] md:text-[14px] md:font-semibold lg:text-[14px]">
