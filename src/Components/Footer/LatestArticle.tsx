423a58a941c373fdd2631bd0ec36d4c11cca8b8d
Pranawa <pranawakc187@gmail.com>
2025-11-19 21:10:37 +0545

    feat(responsive-animation): implement animation

--- src/Components/Footer/LatestArticle.tsx
+++ src/Components/Footer/LatestArticle.tsx
@@ -1,26 +1,10 @@
+import { articles } from "./ArticleData";
 function LatestArticle() {
-  const articles = [
-    {
-      image: "/FarAway.png",
-      heading: "Far far away, behind the word mountains",
-      date: "Mar 16, 2021",
-    },
-    {
-      image: "/CustomizingBrand.jpg",
-      heading: "Customizing your brand and design settings",
-      date: "Mar 16, 2021",
-    },
-    {
-      image: "/WritingAndManaging.jpg",
-      heading: "Writing and managing content in Ghost",
-      date: "Mar 16, 2021",
-    },
-  ];
   return (
     <>
       <div>
         <div className="w-full flex gap-3.5 mb-[30px] items-center">
-          <div className="w-full max-w-[140px] text-[20px] font-sans">
+          <div className="w-full max-w-[140px] text-[20px] font-josefin font-bold text-[#454630]">
             Latest Articles
           </div>
           <div className="w-full border-2 border-red-600  h-0 text-center"></div>
@@ -28,13 +12,23 @@ function LatestArticle() {
         <div className="flex flex-col gap-5 ">
           {articles.map((article, index) => (
             <div key={index} className="flex gap-4">
-              <img
-                src={article.image}
-                alt="image1"
-                className="w-[58px] h-[42px]"
-              />
+              <div className="relative w-[60px] h-[60px] group">
+                <div
+                  className="absolute w-full h-full outline outline-offset-1 "
+                  style={{ outlineColor: article.color }}
+                ></div>
+
+                <div className="absolute w-full h-full flex items-center justify-center transition-all duration-300 group-hover:-top-1.5 group-hover:-left-1.5">
+                  <img
+                    src={article.image}
+                    alt="image1"
+                    className="w-[58px] h-[42px] md:h-[60px] object-cover"
+                  />
+                </div>
+              </div>
+
               <div className="flex flex-col">
-                <div className="text-[12px] text-[#454360]">
+                <div className="text-[12px] text-[#454360] md:text-[13px] font-josefin font-semibold ">
                   {article.heading}
                 </div>
                 <div className="text-[12px]">{article.date}</div>
