3b62ebcb8b60b1eca2513adfb394a5ba7e8f826a
no-routine <amardeeplimbu10@gmail.com>
2025-11-19 15:53:34 +0545

    feat(blogarticle):change filename

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Home } from './components/Home/Home'
+import { Home } from './components/Home/'
 import Membership from './components/Membership/Membership'
 
 function App() {
