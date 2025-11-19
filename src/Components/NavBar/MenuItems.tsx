746a1507ac4a1ab13175561dbd4370bd15d5508c
Pranawa <pranawakc187@gmail.com>
2025-11-19 13:59:28 +0545

    feat(blog-navbar):fix-branchname

--- src/Components/NavBar/MenuItems.tsx
+++ src/Components/NavBar/MenuItems.tsx
@@ -1,30 +1,23 @@
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { menuItems } from "./MenuItemsData";
 function MenuItems() {
   return (
     <>
-      <div className="flex flex-col gap-5 p-6 text-[#4B4870] text-[15px] 
+      <div
+        className="flex flex-col gap-5 p-6 text-[#4B4870] text-[15px] 
        lg:flex-row lg:gap-5 lg:items-center lg:shadow-2xl lg:max-w-[582px] lg:p-0 
-       lg:py-2.5  lg:font-semibold lg:justify-center lg:ml-auto lg:mr-auto">
-        <Link to="/" className="hover:text-red-500">
-          Home
-        </Link>
-        <Link to="/Membership" className="hover:text-red-500">
-          Membership
-        </Link>
-        <Link to="/style-guide" className="hover:text-red-500">
-          Style Guide
-        </Link>
-        <Link to="/tag" className="hover:text-red-500">
-          #Tag
-        </Link>
-        <div className="flex justify-between">
-          <Link to="/more" className="hover:text-red-500">
-            More
-          </Link>
-          <RiArrowDropDownLine className="w-6 h-6" />
-        </div>
+       lg:py-2.5  lg:font-semibold lg:justify-center lg:ml-auto lg:mr-auto"
+      >
+        {menuItems.map((item) => (
+          <div className="flex items-center gap-1" key={item.name}>
+            <Link to={item.path} className="hover:text-red-500">
+              {item.name}
+            </Link>
 
+            {item.hasDropdown && <RiArrowDropDownLine className="w-6 h-6" />}
+          </div>
+        ))}
         <div className="text-center">
           <button className="mt-6 w-[100px] py-2 px-[25px] bg-[#4B4870] text-[15px] text-white lg:mt-0 ">
             Sign In
---
+++ src/Components/NavBar/MenuItemsData.ts
@@ -0,0 +1,7 @@
+export const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "Membership", path: "/Membership" },
+    { name: "Style Guide", path: "/style-guide" },
+    { name: "#Tag", path: "/tag" },
+    { name: "More", path: "/more", hasDropdown: true },
+  ];
