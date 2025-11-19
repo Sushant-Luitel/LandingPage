bf35bbccf6cae75b906cba5014dc589a2d143a72
Pranawa <pranawakc187@gmail.com>
2025-11-19 15:59:23 +0545

    (feat:blog-navbar):fix the file naming

--- src/Components/MainPage/Mainpage.tsx
+++ src/Components/MainPage/Mainpage.tsx
@@ -1,21 +1,31 @@
 import Footer from "../Footer/Footer";
 import { Home } from "../Home/Home";
-import Membership from "../Membership/Membership";
+import Membership from "../Membership/membership-page";
 import NavBar from "../NavBar/NavBar";
 import { Routes, Route } from "react-router-dom";
 function LandingPage() {
   return (
     <>
-      
-        <NavBar />
-        <Routes>
-        <Route path="/" element={<><Home /></>} />
-        </Routes>
-        <Routes>
-        <Route path="/Membership" element={<><Membership /></>} />
-        </Routes>
-        <Footer />
-     
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/Membership"
+          element={
+            <>
+              <Membership />
+            </>
+          }
+        />
+      </Routes>
+      <Footer />
     </>
   );
 }
