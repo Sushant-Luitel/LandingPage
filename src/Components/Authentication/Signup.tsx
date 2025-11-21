1a0a95af671be4237e8c884435fdbd5b13b95857
Pranawa <pranawakc187@gmail.com>
2025-11-21 14:33:17 +0545

    feat(loginPage): fix all the bugs

--- src/Components/Authentication/Signup.tsx
+++ src/Components/Authentication/Signup.tsx
@@ -6,9 +6,9 @@ import { useNavigate } from "react-router-dom";
 type FormValues = z.infer<typeof SignupSchema>;
 
 export const Signup = () => {
-  const navigateToHomePage = useNavigate();
-  const handleClick = () => {
-    navigateToHomePage('/');
+  const navigate = useNavigate();
+  const navigateToHomePage = () => {
+    navigate('/');
   }
   const {
     register,
@@ -27,7 +27,7 @@ export const Signup = () => {
   
   return (
     <div className="md:w-[600px] flex flex-col gap-[30px] justify-center  md:gap-[50px] py-7 px-5 mr-auto ml-auto ">
-      <div onClick={handleClick} className="w-[120px] text-[12px]  font-sans text-[#454360] md:text-[15px] font-semibold text-start hover:underline underline-offset-2 decoration-blue-400 decoration-2">
+      <div onClick={navigateToHomePage} className="w-[120px] text-[12px]  font-sans text-[#454360] md:text-[15px] font-semibold text-start hover:underline underline-offset-2 decoration-blue-400 decoration-2">
         Go back to home
       </div>
       <div className=" xl:w[600px] p-5 shadow-2xl flex rounded-lg mr-auto ml-auto">
